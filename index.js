1ce4e4db3cc3415d070374f4dc9f636fcfec60b7
Min <runechief89@gmail.com>
2022-02-21 22:07:43 -0500

    commit tree 1ba189963b84678f7164824e7629d966ac7be565

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution in this file!
+
+const employee = {
+    key1: "Sam",
+    streetAddress: "123 Road Street",
+};
+
+
+employee;
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    return { ...obj, [key]: value, };
+}
+const employee2 = updateEmployeeWithKeyAndValue(employee, "Gender", "M");
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+    return obj;
+}
+const employee3 = destructivelyUpdateEmployeeWithKeyAndValue(employee, "Hair Color", "Green");
+
+
+function deleteFromEmployeeByKey(obj, key) {
+    const newEmployee = obj;
+    delete newEmployee.key;
+    return newEmployee;
+}
+const employee4 = deleteFromEmployeeByKey(employee, key1);
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete obj.key;
+    return obj;
+}
+const employee5 = destructivelyDeleteFromEmployeeByKey(employee, key1);
